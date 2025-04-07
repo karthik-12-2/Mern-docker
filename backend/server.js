7f568d77f565e46787575cc2bb8ac29d218d0ef9
karthik-12-2 <skarthiklearner@gmail.com>
2025-04-07 18:27:26 +0530

    added docker in this project

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './src/config/db.js';
 import productRoutes from './src/routes/product.route.js';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
@@ -9,12 +10,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(cors());
 
 
 app.use('/api/products', productRoutes);
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT , () => {
   connectDB();
   console.log(`Server is running on http://localhost:${PORT}`)
 });
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -8,25 +8,25 @@ export const useProductStore = create((set) => ({
             if(!newProduct.name || !newProduct.image || !newProduct.price){
                   return {success: false, message: "Please fill in all fields"}
             }
-            const res = await axios.post('/api/products', newProduct);
+            const res = await axios.post('http://localhost:5000/api/products', newProduct);
             set((state) => ({products: [...state.products, res.data.data]}))
             return {success: true, message: "Product created Successfully"};
       },
 
       fetchProducts: async () => {
-            const res = await axios.get('/api/products')
+            const res = await axios.get('http://localhost:5000/api/products')
             set({products: res.data.data})
       },
 
       deleteProducts: async (productId) => {
-            const res = await axios.delete(`/api/products/${productId}`)
+            const res = await axios.delete(`http://localhost:5000/api/products/${productId}`)
             if(!res.data.success) return {success: false, message: res.data.message}
             set(state => ({products: state.products.filter(product => product._id !== productId)}))
             return {success: true, message: res.data.message};
       },
 
       updateProduct: async (productId, updateProduct) => {
-            const res = await axios.put(`/api/products/${productId}`, updateProduct)
+            const res = await axios.put(`http://localhost:5000/api/products/${productId}`, updateProduct)
             if(!res.data.success) return {success: false, message: res.data.message}
             set(state => ({products: state.products.map(product => product._id === productId ? res.data.data : product)}))
             return {success: true, message: res.data.message};
